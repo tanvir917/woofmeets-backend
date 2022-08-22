c6421413975532ddbfc8c50ab444b2451ef90add
M Tanvir Ahmed Rasel <rasel@algosolver.com>
2022-08-22 10:04:22 +0000

    fix: solve folder naming issue in server WOOF-45

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
-import { OAuthDto } from './dto/oauth.dto';
+import { OAuthDto } from './dto/oAuth.dto';
 import { SignupDto } from './dto/signup.dto';
 
 @ApiTags('Auth')
