544e7ad5dd743ede999942022f3c7a970851092b
Fuzail Polin <apolin@algosolver.com>
2022-09-16 06:16:46 +0000

    feat: add one time $35 payment and basic info verification WOOF-165

--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -7,12 +7,20 @@ import {
   UseInterceptors,
   Request,
   Get,
+  UploadedFiles,
 } from '@nestjs/common';
 import { SubscriptionsService } from './subscriptions.service';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { TransformInterceptor } from '../transform.interceptor';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { CreateUserBasicVerificationDto } from './dto/create-user-basic-verification.dto';
 
 @ApiTags('Subscriptions')
 @UseInterceptors(TransformInterceptor)
@@ -53,5 +61,34 @@ export class SubscriptionsController {
   getMySubscriptions(@Request() req: any) {
     const userId = BigInt(req.user.id) ?? BigInt(-1);
     return this.subscriptionsService.getUserSubscription(userId);
+  }
+
+  @ApiOperation({
+    summary: 'Upload user basic verification informaiton. Max 5 images.',
+  })
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FilesInterceptor('file', 5))
+  @Post('user-basic-verification-info')
+  async uploadVerificationInfo(
+    @UploadedFiles() files: Express.Multer.File[],
+    @Body() body: CreateUserBasicVerificationDto,
+    @Request() req: any,
+  ) {
+    const userId = BigInt(req.user?.id) ?? BigInt(-1);
+    return await this.subscriptionsService.createUserBasicVerificationInfo(
+      userId,
+      body,
+      files,
+    );
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  @Get('user-basic-verification-info')
+  async getUserBasicVerificationInfo(@Request() req: any) {
+    const userId = BigInt(req.user?.id) ?? BigInt(-1);
+    return await this.subscriptionsService.getUserBasicVerificationInfo(userId);
   }
 }
