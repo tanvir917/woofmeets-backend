18cf36fb40dad3640d8dddb45402af4fb3a17948
Mohaiminul Hasan Muaz <muaz@algosolver.com>
2022-09-21 16:29:57 +0600

    fix: calendar api availability date calculated WOOF-128

--- src/availability/services/availability.get.service.ts
+++ src/availability/services/availability.get.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { utcToZonedTime, formatInTimeZone } from 'date-fns-tz';
 import { GetAvailableCalenderDto } from 'src/provider/dto/get.available.dto';
 import { throwBadRequestErrorCheck } from 'src/global/exceptions/error-logic';
+import { extractZoneSpecificDateWithFirstHourTime } from 'src/global/time/time-coverters';
 @Injectable()
 export class AvailabilityGetServcie {
   constructor(private readonly prismaService: PrismaService) {}
@@ -17,34 +18,29 @@ export class AvailabilityGetServcie {
      * loop through the range and get all dates
      * get all available days from db
      * select only matched days from all dates
-     * ? get all unavailable dates from db
-     * * get user timezome for opk from db
-     * ! remove all unavailable dates from all dates
-     * * return dates based on provider timezone
+     * get available date and add them on queue
+     * get all unavailable dates from db
+     * get user timezome for opk from db
+     * remove all unavailable dates from all dates
+     * return dates based on provider timezone
      */
 
-    const { startDate, range } = query;
+    const { startDate, endDate } = query;
     const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
     const start = new Date(startDate).getDate();
-    // const lastDate = new Date(
-    //   new Date().getFullYear(),
-    //   new Date().getMonth() + 1,
-    //   0,
-    // ).getDate();
+    const till = new Date(endDate ?? startDate);
 
+    const timediff = Math.floor(
+      (new Date(till).getTime() - new Date(startDate).getTime()) /
+        (24 * 3600 * 1000),
+    );
+    const range = new Date(startDate).getDate() + timediff;
     const end = start + range;
-
     const initDates = [];
-    for (let i = start; i <= end ?? end; i++) {
-      initDates.push(
-        new Date(
-          new Date(new Date().setDate(i)).setHours(
-            new Date(startDate).getHours(),
-          ),
-        ).toISOString(),
-      );
-    }
+    const filtered = [];
+    const timezoned = [];
+    const unavailableDates = [];
 
     const days = await this.prismaService.availableDay.findFirst({
       where: {
@@ -69,48 +65,76 @@ export class AvailabilityGetServcie {
         },
       },
     });
-
     const tz = days.service?.provider?.user?.timezone ?? 'America/New_York';
-
-    const filtered = [];
-    initDates.map((date) => {
-      const getDay = DAYS[new Date(date).getDay()];
-      if (days[getDay]) {
-        filtered.push(date);
-      }
-    });
 
     throwBadRequestErrorCheck(
       opk !== days.service?.provider?.user?.opk,
       'Service not found with specific identifier!',
     );
 
-    const unavailable = await this.prismaService.unavailability.findMany({
+    for (let i = start; i <= end ?? end; i++) {
+      initDates.push(
+        new Date(
+          extractZoneSpecificDateWithFirstHourTime(
+            new Date(new Date().setDate(i)),
+            tz,
+          ),
+        ),
+      );
+    }
+
+    initDates.map((date) => {
+      const getDay = DAYS[new Date(date).getDay()];
+      if (days[getDay]) {
+        filtered.push(date);
+      }
+    });
+    const toDate = initDates[initDates.length - 1];
+
+    /**
+     * get available dates
+     * add them on filtered queue
+     */
+
+    const availability = await this.prismaService.availableDate.findMany({
       where: {
-        userId: days.service?.provider?.user?.id,
+        serviceId,
         deletedAt: null,
         date: {
-          gte: new Date(startDate).toISOString(),
+          gte: new Date(
+            extractZoneSpecificDateWithFirstHourTime(new Date(startDate), tz),
+          ),
+          lte: new Date(
+            extractZoneSpecificDateWithFirstHourTime(new Date(toDate), tz),
+          ),
+        },
+      },
+    });
+    availability.map((a) => {
+      filtered.push(a.date);
+    });
+
+    const unavailable = await this.prismaService.unavailability.findMany({
+      where: {
+        serviceId,
+        deletedAt: null,
+        date: {
+          gte: new Date(
+            extractZoneSpecificDateWithFirstHourTime(new Date(startDate), tz),
+          ),
+          lte: new Date(
+            extractZoneSpecificDateWithFirstHourTime(new Date(toDate), tz),
+          ),
         },
       },
     });
 
-    /**
-     * normalize unavilable dates
-     * if 'allServicesInactive' true then mark it unavialable from the initDates
-     * if 'allServicesInactive' false then check with serviceId
-     */
-
-    const timezoned = [];
-    const unavailableDates = [];
     filtered.map((date) => {
       const availableTime = formatInTimeZone(date, tz, 'yyyy-MM-dd');
       unavailable.map((un) => {
         const ud = formatInTimeZone(un.date, tz, 'yyyy-MM-dd');
         if (ud === availableTime) {
-          if (un.serviceId === BigInt(serviceId)) {
-            unavailableDates.push(ud);
-          }
+          unavailableDates.push(ud);
         }
       });
       timezoned.push(availableTime);
@@ -118,6 +142,7 @@ export class AvailabilityGetServcie {
     const final = timezoned.filter((t) => !unavailableDates.includes(t));
 
     return {
+      message: 'Calendar found successfully.',
       data: {
         timezone: tz,
         dates: final,
