2fc99fa8d35eeabc87d32536dc32a0ae1b82aa0a
Ankur Datta <ankur@algosolver.com>
2022-10-20 10:50:35 +0000

    🎨 refactor: WOOF-361 convert status codes to error logs

--- src/global/config/logger.config.ts
+++ src/global/config/logger.config.ts
@@ -7,14 +7,16 @@ import { createStream } from 'rotating-file-stream';
 const pad = (num: number) => (num > 9 ? '' : '0') + num;
 
 //https://www.npmjs.com/package/rotating-file-stream#:~:text=An%20example%20of%20a%20complex%20rotated%20file%20name%20generator%20function%20could%20be%3A
-const generator = (time?: Date, index?: number): string => {
-  if (!time) return 'error.log';
+const generator =
+  (target: 'info' | 'error' = 'error') =>
+  (time?: Date, index?: number): string => {
+    if (!time) return 'error.log';
 
-  const yearAndMonth = time.getFullYear() + '_' + pad(time.getMonth() + 1);
-  const day = pad(time.getDate());
+    const yearAndMonth = time.getFullYear() + '_' + pad(time.getMonth() + 1);
+    const day = pad(time.getDate());
 
-  return `error_${yearAndMonth}_${day}_${index ?? 1}.log`;
-};
+    return `${target}/${target}_${yearAndMonth}_${day}_${index ?? 1}.log`;
+  };
 
 export const ASYNC_LOGGER_CONFIG: LoggerModuleAsyncParams = {
   useFactory: async (configService: ConfigService) => {
@@ -26,6 +28,14 @@ export const ASYNC_LOGGER_CONFIG: LoggerModuleAsyncParams = {
       renameContext: configService.get<string>('CONTEXT', 'Woofmeets'),
       pinoHttp: {
         redact: ['*.headers'],
+        customLogLevel: function (req, res, err) {
+          if ((res.statusCode >= 400 && res.statusCode >= 500) || err) {
+            return 'error';
+          } else if (res.statusCode >= 300 && res.statusCode < 400) {
+            return 'silent';
+          }
+          return 'info';
+        },
         level: loglevel,
         transport:
           process.env.NODE_ENV !== 'production'
@@ -39,10 +49,17 @@ export const ASYNC_LOGGER_CONFIG: LoggerModuleAsyncParams = {
         stream: pino.multistream([
           {
             level: 'error',
-            // stream: pino.destination(
-            //   `${path.join(__dirname, '..', '..', '..', 'logs', 'error.logs')}`,
-            // ),
-            stream: createStream(generator, {
+            stream: createStream(generator('error'), {
+              path: `${path.join(__dirname, '..', '..', '..', 'logs')}`,
+              interval: '1d',
+              intervalBoundary: true,
+              initialRotation: true,
+              immutable: true,
+            }),
+          },
+          (process.env.STORE_INFO_LOGS || false) && {
+            level: 'info',
+            stream: createStream(generator('info'), {
               path: `${path.join(__dirname, '..', '..', '..', 'logs')}`,
               interval: '1d',
               intervalBoundary: true,
