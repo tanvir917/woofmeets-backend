43ac73674f78205a494efaf56cdb41cc61066a8b
M Tanvir Ahmed Rasel <rasel@algosolver.com>
2022-11-04 11:08:23 +0000

    fix: WOOF-388 mailgun email domain server error issue

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { MailgunService } from '@nextnm/nestjs-mailgun';
+import { PinoLogger } from 'nestjs-pino';
 import { SecretService } from 'src/secret/secret.service';
 import { TestEmailDto } from './dto/test.email.dto';
 import { ForgetPasswordTemplate } from './template/forget-password.template.dto';
@@ -13,148 +14,196 @@ export class EmailService {
   constructor(
     private mailgunService: MailgunService,
     private secretService: SecretService,
-  ) {}
+    private logger: PinoLogger,
+  ) {
+    this.logger.setContext(EmailService.name);
+  }
 
   async sendEmail(param: TestEmailDto) {
-    const { email, subject, message } = param;
-    const content = new ForgetPasswordTemplate('').html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: subject,
-        text: message,
-        html: content,
-      },
-    );
+    try {
+      const { email, subject, message } = param;
+      const content = new ForgetPasswordTemplate('').html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: subject,
+          text: message,
+          html: content,
+        },
+      );
+    } catch (error) {
+      return null;
+    }
   }
 
   async updatePasswordConfirmEmail(email: string, name?: string) {
-    const content = new UpdatePasswordTemplate(name).html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Password changed for Woofmeets account.',
-        html: content,
-      },
-    );
+    try {
+      const content = new UpdatePasswordTemplate(name).html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Password changed for Woofmeets account.',
+          html: content,
+        },
+      );
+    } catch (error) {
+      return null;
+    }
   }
 
   async forgetPasswordOTPEmail(email: string, otp?: string) {
-    const content = new ForgetPasswordTemplate(otp).html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Reset Password for Woofmeets account.',
-        html: content,
-      },
-    );
+    try {
+      const content = new ForgetPasswordTemplate(otp).html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Reset Password for Woofmeets account.',
+          html: content,
+        },
+      );
+    } catch (error) {
+      return null;
+    }
   }
 
   async signupWelcomeEmail(email: string) {
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Welcome to Woofmeets',
-        template: 'cms_woofmeets',
-      },
-    );
+    try {
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Welcome to Woofmeets',
+          template: 'cms_woofmeets',
+        },
+      );
+    } catch (error) {
+      //console.log(error?.message);
+      this.logger.error(error?.message);
+    }
   }
 
   async completeOnBoardingEmail(email: string) {
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Your woofmeets journey begins from here.',
-        template: 'signup_welcome',
-      },
-    );
+    try {
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Your woofmeets journey begins from here.',
+          template: 'signup_welcome',
+        },
+      );
+    } catch (error) {
+      this.logger.error(error?.message);
+    }
   }
 
   async appointmentAcceptEmail(email: string, status?: string) {
-    const content = new TempAppointmentTemplate(status).html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Appointment accepted notification',
-        html: content,
-      },
-    );
+    try {
+      const content = new TempAppointmentTemplate(status).html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Appointment accepted notification',
+          html: content,
+        },
+      );
+    } catch (error) {
+      this.logger.error(error?.message);
+    }
   }
 
   async appointmentCreationEmail(email: string, status?: string) {
-    const content = new TempAppointmentTemplate(status).html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'New appointment notification',
-        html: content,
-      },
-    );
+    try {
+      const content = new TempAppointmentTemplate(status).html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'New appointment notification',
+          html: content,
+        },
+      );
+    } catch (error) {
+      this.logger.error(error?.message);
+    }
   }
 
   async appointmentPaymentEmail(email: string, amount: number, txnId: string) {
-    const content = new PaymentAppointmentTemplate({ amount, txnId }).html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Appointment payment notification',
-        html: content,
-      },
-    );
+    try {
+      const content = new PaymentAppointmentTemplate({ amount, txnId }).html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Appointment payment notification',
+          html: content,
+        },
+      );
+    } catch (error) {
+      this.logger.error(error?.message);
+    }
   }
 
   async appointmentPaymentForProviderEmail(email: string) {
-    const content = new PaymentAppointmentForProviderTemplate().html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Appointment payment notification',
-        html: content,
-      },
-    );
+    try {
+      const content = new PaymentAppointmentForProviderTemplate().html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Appointment payment notification',
+          html: content,
+        },
+      );
+    } catch (error) {
+      this.logger.error(error?.message);
+    }
   }
 
   async appointmentCancelEmail(email: string, status?: string) {
-    const content = new TempAppointmentTemplate(status).html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Appointment cancelled notification',
-        html: content,
-      },
-    );
+    try {
+      const content = new TempAppointmentTemplate(status).html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Appointment cancelled notification',
+          html: content,
+        },
+      );
+    } catch (error) {
+      this.logger.error(error?.message);
+    }
   }
 
   async appointmentCompleteEmail(email: string, status?: string) {
-    const content = new TempAppointmentTemplate(status).html();
-    return this.mailgunService.createEmail(
-      this.secretService.getMailgunCreds().domain,
-      {
-        from: this.secretService.getMailgunCreds().from,
-        to: email,
-        subject: 'Appointment completed notification',
-        html: content,
-      },
-    );
+    try {
+      const content = new TempAppointmentTemplate(status).html();
+      return this.mailgunService.createEmail(
+        this.secretService.getMailgunCreds().domain,
+        {
+          from: this.secretService.getMailgunCreds().from,
+          to: email,
+          subject: 'Appointment completed notification',
+          html: content,
+        },
+      );
+    } catch (error) {
+      this.logger.error(error?.message);
+    }
   }
 }
