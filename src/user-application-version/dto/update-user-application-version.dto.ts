4dc8bad3d178f11785f69e3eedcc75016de0600f
Abu Fuzail Polin <apolin@algosolver.com>
2022-12-07 16:47:43 +0600

    refactor: add optional property in dto

--- src/user-application-version/dto/update-user-application-version.dto.ts
+++ src/user-application-version/dto/update-user-application-version.dto.ts
@@ -1,33 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserApplicationVersionDto {
   @ApiProperty()
   @IsString()
-  @IsNotEmpty()
-  version: string;
+  @IsOptional()
+  version?: string;
 
   @ApiProperty()
   @IsString()
-  @IsOptional()
   iosForceUpdateVersion: string;
 
   @ApiProperty()
   @IsString()
-  @IsOptional()
   androidForceUpdateVersion: string;
 
   @ApiProperty()
   @IsString()
-  @IsOptional()
   androidStoreUrl: string;
 
   @ApiProperty()
   @IsString()
-  @IsOptional()
   iosStoreUrl: string;
 
   @ApiProperty()
   @IsOptional()
-  meta: object;
+  meta?: object;
 }
