11c2abd2f70da394f77ee6e9592cb7b071aeda7b
Fuzail Polin <apolin@algosolver.com>
2022-12-09 10:52:54 +0000

    ✨ feat: WOOF-539 create services for pet sitter payment history

--- src/balances/balances.service.ts
+++ src/balances/balances.service.ts
@@ -88,4 +88,96 @@ export class BalancesService {
       },
     };
   }
+
+  async findPetSitterPaymentHistory(
+    userId: bigint,
+    query: GetBalancesQueryDto,
+  ) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: userId,
+      },
+      include: {
+        provider: true,
+      },
+    });
+
+    throwBadRequestErrorCheck(!user, 'User not found');
+    throwBadRequestErrorCheck(!user.provider, 'Provider not found');
+
+    let { page, limit, sortBy, sortOrder } = query;
+    const { status } = query;
+    const orderbyObj = {};
+
+    const statusQuery = BalanceStatus[status]
+      ? { status: BalanceStatus[status] }
+      : {};
+    if (!page || page < 1) page = 1;
+    if (!limit) limit = 20;
+    if (!sortOrder && sortOrder != 'asc' && sortOrder != 'desc')
+      sortOrder = 'desc';
+    if (!sortBy) sortBy = 'createdAt';
+
+    orderbyObj[sortBy] = sortOrder;
+
+    const [count, paymentHistory] = await this.prismaService.$transaction([
+      this.prismaService.appointmentBillingPayments.count({
+        where: {
+          billing: {
+            appointment: {
+              providerId: user?.provider?.id,
+            },
+          },
+          ...statusQuery,
+        },
+      }),
+
+      this.prismaService.appointmentBillingPayments.findMany({
+        where: {
+          billing: {
+            appointment: {
+              providerId: user?.provider?.id,
+            },
+          },
+          ...statusQuery,
+        },
+        include: {
+          billing: {
+            include: {
+              appointment: {
+                include: {
+                  appointmentPet: true,
+                },
+              },
+            },
+          },
+          paidByUser: {
+            select: {
+              id: true,
+              firstName: true,
+              lastName: true,
+              email: true,
+              image: true,
+            },
+          },
+        },
+        orderBy: orderbyObj,
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+    ]);
+
+    throwBadRequestErrorCheck(count === 0, 'No payment history found');
+
+    return {
+      message: 'Payment history found',
+      data: paymentHistory,
+      meta: {
+        total: count,
+        currentPage: page,
+        limit,
+        stripePayoutFee: 1,
+      },
+    };
+  }
 }
